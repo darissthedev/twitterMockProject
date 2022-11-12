42c2bca8fbe89333c308bac48d77f4ea40f31349
Dariss Harris <67239834+darissthedev@users.noreply.github.com>
2022-11-12 19:59:49 +0100

    Code added (like, unlike)
    User are able to like and unlike tweets. Increment and decrement enabled.

--- twitterMockProject/index.js
+++ twitterMockProject/index.js
@@ -8,29 +8,57 @@ tweetBtn.addEventListener('click', function(){
     console.log(tweetInput.value)
 })
 
+document.addEventListener('click', function(e){
+    if (e.target.dataset.like){
+       handleLikeClick(e.target.dataset.like) 
+    }
+})
+
+function handleLikeClick(tweetId){
+    const targetTweetObj = tweetsData.filter(function (tweet){
+        return tweet.uuid === tweetId
+    })[0]
+
+    if (targetTweetObj.isLiked){
+        targetTweetObj.likes--
+        targetTweetObj.isLiked = false
+    } else {
+        targetTweetObj.likes++
+        targetTweetObj.isLiked = true
+    }
+
+    render()
+}
+
 function getFeedHtml(){
     let feedHtml = ``
     tweetsData.forEach(function(tweet){
         feedHtml += `
-        <div class="tweet">
-        <div class="tweet-inner">
-        <img src="${tweet.profilePic}" class="profile-pic">
-        <div>
-            <p class="handle">${tweet.handle}</p>
-            <p class="tweet-text">${tweet.tweetText}</p>
-            <div class="tweet-details">
-                <span class="tweet-detail">
-                <i class="fa-regular fa-comment-dots"></i>
-                    ${tweet.replies.length}
-                </span>
-                <span class="tweet-detail">
-                <i class="fa-solid fa-heart"></i>
-                    ${tweet.likes}
-                </span>
-                <span class="tweet-detail">
-                <i class="fa-solid fa-retweet"></i>
-                    ${tweet.retweets}
-                </span>
+                <div class="tweet">
+            <div class="tweet-inner">
+                <img src="${tweet.profilePic}" class="profile-pic">
+                <div>
+                    <p class="handle">${tweet.handle}</p>
+                    <p class="tweet-text">${tweet.tweetText}</p>
+                    <div class="tweet-details">
+                        <span class="tweet-detail">
+                            <i class="fa-regular fa-comment-dots"
+                            data-reply="${tweet.uuid}"
+                            ></i>
+                            ${tweet.replies.length}
+                        </span>
+                        <span class="tweet-detail">
+                            <i class="fa-solid fa-heart"
+                            data-like="${tweet.uuid}"
+                            ></i>
+                            ${tweet.likes}
+                        </span>
+                        <span class="tweet-detail">
+                            <i class="fa-solid fa-retweet"
+                            data-retweet="${tweet.uuid}"
+                            ></i>
+                            ${tweet.retweets}
+                        </span>
                     </div>   
                 </div>            
             </div>
@@ -39,8 +67,6 @@ function getFeedHtml(){
     })
     return feedHtml
 }
-
-getFeedHtml()
 
 function render(){
     document.getElementById('feed').innerHTML = getFeedHtml()
