687cc19fd462fe37e69c26ab48c5fafcadc6d71f
Dariss Harris <67239834+darissthedev@users.noreply.github.com>
2022-11-13 22:06:26 +0100

    updated code to enable like

--- twitterMockProject/index.js
+++ twitterMockProject/index.js
@@ -21,12 +21,10 @@ function handleLikeClick(tweetId){
 
     if (targetTweetObj.isLiked){
         targetTweetObj.likes--
-        targetTweetObj.isLiked = false
     } else {
         targetTweetObj.likes++
-        targetTweetObj.isLiked = true
     }
-
+    targetTweetObj.isLiked = !targetTweetObj.isLiked
     render()
 }
 
