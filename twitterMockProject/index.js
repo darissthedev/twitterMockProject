2ef563505a4dda89ff410a09cce9761e2d8fdddd
Dariss Harris <67239834+darissthedev@users.noreply.github.com>
2022-11-14 23:58:27 +0100

    Linked class style for UI
    Added functionality to 'like' and 'retweet' classes to style.

--- twitterMockProject/index.js
+++ twitterMockProject/index.js
@@ -48,6 +48,14 @@ function handleRetweetClick(tweetId){
 function getFeedHtml(){
     let feedHtml = ``
     tweetsData.forEach(function(tweet){
+        let likeIconClass = ''
+        if(tweet.isLiked){
+            likeIconClass = 'liked'
+        }
+        let retweetIconClass = ''
+        if(tweet.isRetweeted){
+            retweetIconClass = 'retweeted'
+        }
         feedHtml += `
                 <div class="tweet">
             <div class="tweet-inner">
@@ -63,13 +71,13 @@ function getFeedHtml(){
                             ${tweet.replies.length}
                         </span>
                         <span class="tweet-detail">
-                            <i class="fa-solid fa-heart"
+                            <i class="fa-solid fa-heart ${likeIconClass}"
                             data-like="${tweet.uuid}"
                             ></i>
                             ${tweet.likes}
                         </span>
                         <span class="tweet-detail">
-                            <i class="fa-solid fa-retweet"
+                            <i class="fa-solid fa-retweet ${retweetIconClass}"
                             data-retweet="${tweet.uuid}"
                             ></i>
                             ${tweet.retweets}
